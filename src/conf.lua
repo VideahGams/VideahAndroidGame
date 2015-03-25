4ce7ffd8b42512a709eb37876d4b1e80e9facf23
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-03-25 17:06:07 +0000

    Let's start working on this thing

--- src/conf.lua
+++ src/conf.lua
@@ -2,12 +2,12 @@ io.stdout:setvbuf("no") -- Prints to SublimeText's console
 
 function love.conf(c)
 
-	c.title = "VideahEngine"
+	c.title = "VideahMobileGame"
 	c.author = "Ruairidh 'VideahGams' Carmichael"
-	c.identity = "VideahEngine"
+	c.identity = "VideahMobileGame"
 
-	c.window.width = 1280
-	c.window.height = 720
+	c.window.width = 640
+	c.window.height = 1136
 	c.window.resizable = true
 
 end
